44afbe2b818f8cc6650c46f896c87d71daeb282c
Ravi-vira <vira5642@gmail.com>
2025-01-08 10:56:08 +0530

    file add

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+// Task 1
+
+// import ollama from "ollama";
+// const response = await ollama.chat({
+//     model: "llama3.1:8b",
+//     messages: [{ role: "user", content: "Generate marketing emails" }],
+// });
+// console.log(response.message.content);
+
+//task 2
 import ollama from "ollama";
-const response = await ollama.chat({
-    model: "llama3.1:8b",
-    messages: [{ role: "user", content: "Generate marketing emails" }],
-});
-console.log(response.message.content);
+import fs from "fs";
+
+let q= fs.readFileSync("./q.txt", "utf-8");
+console.log(q)
+
+askQuestion()
+async function askQuestion() {
+  try {
+    const response = await ollama.chat({
+      model: "llama3.2:latest",
+      messages: [{ role: 'user', content: q }]
+    });
+
+    fs.writeFileSync("./a.txt", response.message.content);
+
+  } catch (error) {
+    console.error("Error occurred:", error.message);
+  }
+}
