cd0b73bc2034829941afc3eeae02ec0f926d2673
Ravi-vira <vira5642@gmail.com>
2025-01-08 13:13:18 +0530

    filw

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//task 1
+// import ollama from "ollama";
+
+// async function runChat() {
+//   try {
+//     const response = await ollama.chat({
+//       model: "llama3.2:latest",
+//       messages: [{ role: 'user', content: "Generate marketing emails" }]
+//     });
+
+//     console.log("Chatbot Response:", response.message.content);
+//   } catch (error) {
+//     console.error("Error occurred:", error.message);
+//   }
+// }
+
+// runChat();
+
+
+
+
 import ollama from "ollama";
 import fs from "fs";
 
